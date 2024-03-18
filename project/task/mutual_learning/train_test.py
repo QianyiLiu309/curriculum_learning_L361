2d5011bf6bd548e85b29d4a9e26f0a6bab3ca1e7
liuqianyi <Qianyi Liu>
2024-03-18 20:49:46 +0000

    Update mutual learning with the new curriculum implementation

--- project/task/mutual_learning/train_test.py
+++ project/task/mutual_learning/train_test.py
@@ -11,7 +11,9 @@ from torch import nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 
-from project.task.self_paced_learning.pacing_utils import get_loss_threshold
+from project.task.self_paced_learning.pacing_utils import (
+    get_filtered_trainloader,
+)
 
 from project.task.default.train_test import get_fed_eval_fn as get_default_fed_eval_fn
 from project.task.default.train_test import (
@@ -34,11 +36,153 @@ class TrainConfig(BaseModel):
     epochs: int
     learning_rate: float
     percentage: float | None
+    is_anti: bool | None
 
     class Config:
         """Setting to allow any types, including library ones like torch.device."""
 
         arbitrary_types_allowed = True
+
+
+# def train(  # pylint: disable=too-many-arguments
+#     net: nn.Module,
+#     local_net: nn.Module,
+#     trainloader: DataLoader,
+#     _config: dict,
+#     _working_dir: Path,
+#     _rng_tuple: IsolatedRNG,
+# ) -> tuple[int, dict]:
+#     """Train the network on the training set.
+
+#     Parameters
+#     ----------
+#     net : nn.Module
+#         The neural network to train.
+#     trainloader : DataLoader
+#         The DataLoader containing the data to train the network on.
+#     _config : Dict
+#         The configuration for the training.
+#         Contains the device, number of epochs and learning rate.
+#         Static type checking is done by the TrainConfig class.
+#     _working_dir : Path
+#         The working directory for the training.
+#         Unused.
+#     _rng_tuple : IsolatedRNGTuple
+#         The random number generator state for the training.
+#         Use if you need seeded random behavior
+
+#     Returns
+#     -------
+#     Tuple[int, Dict]
+#         The number of samples used for training,
+#         the loss, and the accuracy of the input model on the given data.
+#     """
+#     if len(cast(Sized, trainloader.dataset)) == 0:
+#         raise ValueError(
+#             "Trainloader can't be 0, exiting...",
+#         )
+#     config: TrainConfig = TrainConfig(**_config)
+#     del _config
+
+#     net.to(config.device)
+#     local_net.to(config.device)
+
+#     frozen_teacher_net = copy.deepcopy(net)
+#     frozen_teacher_net.to(config.device)
+#     for param in frozen_teacher_net.parameters():
+#         param.requires_grad = False
+
+#     loss_threshold = get_loss_threshold(
+#         frozen_teacher_net,
+#         trainloader,
+#         config.percentage,
+#         config.device,
+#     )
+#     print(f"loss_threshold based on frozen teacher: {loss_threshold}")
+
+#     criterion = nn.CrossEntropyLoss(reduction="none")
+#     kl_div_loss = nn.KLDivLoss(reduction="none")
+#     optimizer = torch.optim.SGD(
+#         net.parameters(),
+#         lr=config.learning_rate,
+#     )
+#     optimiser_local = torch.optim.SGD(local_net.parameters(), lr=config.learning_rate)
+
+#     final_epoch_per_sample_loss = 0.0
+#     num_correct = 0
+#     final_epoch_per_sample_loss_local = 0.0
+#     num_correct_local = 0
+#     for i in range(config.epochs):
+#         net.train()
+#         local_net.train()
+#         final_epoch_per_sample_loss = 0.0
+#         num_correct = 0
+#         final_epoch_per_sample_loss_local = 0.0
+#         num_correct_local = 0
+#         for data, target in trainloader:
+#             data, target = (
+#                 data.to(
+#                     config.device,
+#                 ),
+#                 target.to(config.device),
+#             )
+
+#             with torch.no_grad():
+#                 teacher_output = frozen_teacher_net(data)
+#                 teacher_losses = criterion(teacher_output, target)
+
+#             # only learn on samples with loss < loss_threshold
+#             mask = teacher_losses <= loss_threshold
+#             output = net(data)
+#             output_local = local_net(data)
+
+#             kl_div_to_net = kl_div_loss(
+#                 F.log_softmax(output, dim=1),
+#                 F.softmax(output_local.detach(), dim=1),
+#             ).sum(dim=1)
+#             kl_div_to_local_net = kl_div_loss(
+#                 F.log_softmax(output_local, dim=1),
+#                 F.softmax(output.detach(), dim=1),
+#             ).sum(dim=1)
+
+#             losses = criterion(output, target) + kl_div_to_net
+#             losses_local = criterion(output_local, target) + kl_div_to_local_net
+
+#             loss = (losses * mask).sum() / (mask.sum() + 1e-10)
+#             loss_local = (losses_local * mask).sum() / (mask.sum() + 1e-10)
+
+#             optimizer.zero_grad()
+#             loss.backward()
+#             optimizer.step()
+
+#             optimiser_local.zero_grad()
+#             loss_local.backward()
+#             optimiser_local.step()
+
+#             final_epoch_per_sample_loss += loss.item()
+#             num_correct += (output.max(1)[1] == target).clone().detach().sum().item()
+
+#             final_epoch_per_sample_loss_local += loss.item()
+#             num_correct_local += (
+#                 (output_local.max(1)[1] == target).clone().detach().sum().item()
+#             )
+#         print(
+#             f"Epoch {i + 1}, loss:"
+#             f" {final_epoch_per_sample_loss / len(trainloader.dataset)}, accuracy:"
+#             f" {num_correct / len(trainloader.dataset)}, local loss"
+#             f" {final_epoch_per_sample_loss_local / len(trainloader.dataset)},"
+#             f" local accuracy: {num_correct_local / len(trainloader.dataset)}"
+#         )
+
+#     return len(cast(Sized, trainloader.dataset)), {
+#         "train_loss": final_epoch_per_sample_loss
+#         / len(cast(Sized, trainloader.dataset)),
+#         "train_accuracy": float(num_correct) / len(cast(Sized, trainloader.dataset)),
+#         "local_train_loss": final_epoch_per_sample_loss_local
+#         / len(cast(Sized, trainloader.dataset)),
+#         "local_train_accuracy": float(num_correct_local)
+#         / len(cast(Sized, trainloader.dataset)),
+#     }
 
 
 def train(  # pylint: disable=too-many-arguments
@@ -89,16 +233,20 @@ def train(  # pylint: disable=too-many-arguments
     for param in frozen_teacher_net.parameters():
         param.requires_grad = False
 
-    loss_threshold = get_loss_threshold(
+    trainloader_filtered = get_filtered_trainloader(
         frozen_teacher_net,
         trainloader,
         config.percentage,
         config.device,
+        ascending_order=not config.is_anti,
     )
-    print(f"loss_threshold based on frozen teacher: {loss_threshold}")
+    print(
+        "Length of trainloader after filtering:"
+        f" {len(cast(Sized, trainloader_filtered.dataset))}"
+    )
 
-    criterion = nn.CrossEntropyLoss(reduction="none")
-    kl_div_loss = nn.KLDivLoss(reduction="none")
+    criterion = nn.CrossEntropyLoss()
+    kl_div_loss = nn.KLDivLoss(reduction="mean")
     optimizer = torch.optim.SGD(
         net.parameters(),
         lr=config.learning_rate,
@@ -124,29 +272,20 @@ def train(  # pylint: disable=too-many-arguments
                 target.to(config.device),
             )
 
-            with torch.no_grad():
-                teacher_output = frozen_teacher_net(data)
-                teacher_losses = criterion(teacher_output, target)
-
-            # only learn on samples with loss < loss_threshold
-            mask = teacher_losses <= loss_threshold
             output = net(data)
             output_local = local_net(data)
 
             kl_div_to_net = kl_div_loss(
                 F.log_softmax(output, dim=1),
                 F.softmax(output_local.detach(), dim=1),
-            ).sum(dim=1)
+            )
             kl_div_to_local_net = kl_div_loss(
                 F.log_softmax(output_local, dim=1),
                 F.softmax(output.detach(), dim=1),
-            ).sum(dim=1)
+            )
 
-            losses = criterion(output, target) + kl_div_to_net
-            losses_local = criterion(output_local, target) + kl_div_to_local_net
-
-            loss = (losses * mask).sum() / (mask.sum() + 1e-10)
-            loss_local = (losses_local * mask).sum() / (mask.sum() + 1e-10)
+            loss = criterion(output, target) + kl_div_to_net
+            loss_local = criterion(output_local, target) + kl_div_to_local_net
 
             optimizer.zero_grad()
             loss.backward()

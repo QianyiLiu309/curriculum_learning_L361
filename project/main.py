c59b0ca2c9a241c0b351fb70b6fb481677801772
liuqianyi <Qianyi Liu>
2024-03-19 14:10:14 +0000

    Add seed into experiment name, unify wandb project name

--- project/main.py
+++ project/main.py
@@ -109,7 +109,7 @@ def main(cfg: DictConfig) -> None:
     # Wandb context manager
     # controls if wandb is initialized or not
     # if not it returns a dummy run
-    wandb_name = f"{cfg.task.model_and_data}_{cfg.strategy.init.curriculum_strategy}_{cfg.task.fit_config.run_config.epochs}_{cfg.dataset.name}_{cfg.dataset.alpha}"
+    wandb_name = f"{cfg.task.model_and_data}_{cfg.strategy.init.curriculum_strategy}_{cfg.task.fit_config.run_config.epochs}_{cfg.dataset.name}_{cfg.dataset.alpha}_{cfg.fed.seed}"
     with wandb_init(
         cfg.use_wandb,
         **cfg.wandb.setup,
